d889e389357da3082321a9fd1db336c0c4fd0c1a
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 15:25:50 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,7 +3,7 @@ require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
  i = 0 
  coupons.each do |coupon|
-   item_with_coupon = find_
+   item_with_coupon = find_item_
 end
 
 def apply_clearance(cart)
