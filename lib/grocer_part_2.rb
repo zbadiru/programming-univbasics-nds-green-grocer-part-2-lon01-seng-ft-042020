c9da84e5137517f5c6eea7c0775b3769d7ababd8
Zakari Badiru <zbadiru@live.co.uk>
2020-04-08 15:33:37 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
  coupons.each do |coupon|
    item_with_coupon = find_item_by_name_in_collection(coupon[:item],cart)
    item_is_in_basket = !!item_with_coupon
-   count_is_big = item_is_in_basket && item_with_coupon[:count]
+   count_is_big = item_is_in_basket && item_with_coupon[:count] >=
 end
 
 def apply_clearance(cart)
